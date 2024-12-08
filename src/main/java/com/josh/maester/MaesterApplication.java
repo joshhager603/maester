fc13e5b11eb1828dc9cf6925d81b31759f6ff979
Josh Hager <jhager603@gmail.com>
2024-12-07 20:53:59 -0500

    intial commit

---
+++ src/main/java/com/josh/maester/MaesterApplication.java
@@ -0,0 +1,13 @@
+package com.josh.maester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josh/maester/MaesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.maester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
